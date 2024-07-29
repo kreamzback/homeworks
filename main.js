278aac60b074a58d92820523747d3495f55a7c29
kreamzback <egorkrez555@gmail.com>
2024-07-29 14:40:30 +0300

    homework_02

--- main.js
+++ main.js
@@ -1,1 +1,193 @@
-console.log("Hey, this is my first JS program");
+//let a = 2;
+//let b = 3;
+//let c = a+b;
+
+//console.log(c + 3);
+
+// String
+//let greeting = "Hello!";
+
+// console.log(greeting);
+// console.log(greeting.lenght);
+// console.log("Hello!".lenght);
+// console.log(greeting[2]);
+// console.log("Hello!"[2]);
+
+// Number
+//let num1 = 100; // int
+//let p = 3.1415926; // float
+
+//console.log(num1 * p);
+//console.log(Infinity); // 1 / 0 = Infinity
+//console.log(-Infinity);
+//console.log(NaN); // "Привіт" * 4 = NaN;
+
+// Boolean
+// console.log(true, false);
+// console.log(12 > 15);
+
+// // undefined
+// let a = 10;
+
+// a = undefined;
+
+// console.log(a)
+
+// // null
+// let b = null;
+
+// console.log(b);
+
+// BigInt
+// let bigInt = 123n;
+// console.log(bigInt);
+
+// Symbol
+// let a = Symbol("id1");
+// let b = Symbol("id1");
+// console.log(a === b);
+
+// Object
+// let user = {
+//   name: "John",
+//   phone: 1337228,
+//   isAdmin: true,
+// };
+
+// console.log(user.name);
+// console.log(user.phone);
+// console.log(user["isAdmin"]);
+
+// array 
+// let cars = ["maybach", "BMW", "Mersedes"];
+
+// console.log(cars);
+
+// cars[1] = "Ferrari";
+
+// console.log(cars);
+
+// function
+
+// function sayHi(name) {
+//   console.log(`Hi ${name}`);
+// };
+
+// sayHi("Egor");
+
+// typeof
+
+// console.log(typeof "123"); // String
+
+// Boolean()
+// "", 0, 0n, null, undefined, NaN
+
+// console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(0n));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+// console.log(Boolean("==========="));
+// console.log(Boolean(" "));
+// console.log(Boolean(123));
+// console.log(Boolean({}));
+
+
+// Number()
+
+// console.log(Number("     100    "));
+// console.log(Number("100"));
+// console.log(Number("100px"));
+
+//parseInt / parseFloat
+// console.log(parseInt("100px"));
+// console.log(parseFloat("150.5px"));
+
+// String()
+// let num1 = 123;
+// let str1 = String(num1);
+// console.log(typeof num1);
+// console.log(str1);
+// console.log(typeof str1);
+
+// console.log(5 + 3);
+// console.log(5 - 3);
+// console.log(5 / 3);
+// console.log(5 * 3);
+// console.log(5 ** 3);
+// console.log(4 ** 2);
+
+// console.log(7 % 3); 
+// console.log(8 % 3);
+// console.log(5 % 2);
+// console.log(10 % 2);
+// console.log(11 % 3);
+
+// let a = 10;
+// a = a + 2;
+// a += 2; 
+
+// console.log(a)
+
+// console.log(--a); // Decrement
+// console.log(++a); // Increment
+
+
+
+// > < <= >= == != === !==
+
+// let a = 5; 
+// let b = 10;
+// let c = 5
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a <= c);
+// console.log(a < c);
+// console.log(a == b);
+
+// console.log("" > 2); // false because "" === 0
+
+// console.log(null == undefined);
+// console.log(null > 0); //false
+// console.log(null >= 0); //true
+// console.log(null == 0); //false
+
+// console.log(undefined > 0);
+// console.log(NaN > 0);
+
+// нет !
+
+
+// или ||
+// let time = 7;
+
+// Зачинений магазин?
+// console.log(time < 8 || time > 20);
+// console.log(true || false);
+// let phone = true;
+// let email = false;
+// console.log(phone || email);
+
+// и &&
+// let hours = 8;
+// let minutes = 49;
+
+// console.log(hours === 8 && minutes === 50);
+
+// console.log(!true);
+// console.log(!false);
+
+// console.log(!10); // !true
+
+// ?
+
+// let age = 19;
+// умова ? true : false;
+// let result = (age > 18) ? "Дозволено" : "Відхилено";
+
+// console.log(result);
+
+console.log((+null == false) < 1 ** 5);
